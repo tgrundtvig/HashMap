b8e5153f047fa99df8503015553d0db2353a9466
jonathanec94 <jona_egebak@hotmail.dk>
2017-03-29 15:25:23 +0200

    Nikoali Hansen & Jonathan

--- src/hashmap/Test.java
+++ src/hashmap/Test.java
@@ -11,21 +11,21 @@ package hashmap;
  */
 public class Test
 {
-    public static void main(String[] args)
+   public static void main(String[] args)
     {
-        Map<String, Integer> myMap = new HashMapLink<>();
+        //Map<String, Integer> myMap = new HashMapLink<>();
+        Map<String, Integer> myMap = new HashMap<>();
         
         for(int i = 0; i < 1000; i += 2)
         {
             String s = "#" + i;
             myMap.put(s, i);
         }
-        System.out.println("Size: " + myMap.size());
-        for(int i = 0; i < 1000; ++i)
-        {
-            String s = "#" + i;
-            Integer value = myMap.get(s);
-            System.out.println("Key: " + s + " Value: " + value);
-        }
+
+        
+        System.out.println("Test remove link: "+myMap.get("#2"));
+        myMap.remove("#2");
+        System.out.println("Test remove link: "+myMap.get("#2"));
+        
     }
 }
