415c75eea101d4d7f74b5abdd1d4a54fa53207a1
KSWorm <kasper@sylvestworm.dk>
2017-03-30 13:19:06 +0200

    refactor(Formatting): Formatted code with netbeans

--- src/hashmap/Test.java
+++ src/hashmap/Test.java
@@ -9,20 +9,17 @@ package hashmap;
  *
  * @author Tobias
  */
-public class Test
-{
-    public static void main(String[] args)
-    {
+public class Test {
+
+    public static void main(String[] args) {
         Map<String, Integer> myMap = new HashMapLink<>();
-        
-        for(int i = 0; i < 1000; i += 2)
-        {
+
+        for (int i = 0; i < 1000; i += 2) {
             String s = "#" + i;
             myMap.put(s, i);
         }
         System.out.println("Size: " + myMap.size());
-        for(int i = 0; i < 1000; ++i)
-        {
+        for (int i = 0; i < 1000; ++i) {
             String s = "#" + i;
             Integer value = myMap.get(s);
             System.out.println("Key: " + s + " Value: " + value);
