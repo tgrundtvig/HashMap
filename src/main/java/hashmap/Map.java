3fe68c75f864f103aa7ed9222449ed2d2f7c5f82
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-03-30 23:12:34 +0200

    Formatted code

--- src/main/java/hashmap/Map.java
+++ src/main/java/hashmap/Map.java
@@ -6,17 +6,19 @@
 package hashmap;
 
 /**
- *
  * @author Tobias
  */
-public interface Map<K, V>
-{
-   public V put(K key, V value);
-   public V get(K key);
-   public V remove(K key);
-   public int size();
-   public default boolean isEmpty()
-   {
-       return size() == 0;
-   }
+public interface Map<K, V> {
+
+    public V put(K key, V value);
+
+    public V get(K key);
+
+    public V remove(K key);
+
+    public int size();
+
+    public default boolean isEmpty() {
+        return size() == 0;
+    }
 }
