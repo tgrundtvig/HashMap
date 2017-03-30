9af11a0f7288b43b5e8fd3c339ad8d8110cf5330
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-03-30 23:24:45 +0200

    Added delete to HashMap

--- src/main/java/hashmap/HashMap.java
+++ src/main/java/hashmap/HashMap.java
@@ -44,25 +44,40 @@ public class HashMap<K, V> implements Map<K, V> {
     @Override
     public V get(K key) {
         int index = getIndex(key);
-        int collisions = 0;
         while (true) {
 
             if (array[index] == null) {
-                System.out.println("Collisions: " + collisions);
                 return null;
             } else if (array[index].getKey().equals(key)) {
-                System.out.println("Collisions: " + collisions);
                 return array[index].getValue();
             } else {
                 index = incIndex(index);
-                ++collisions;
             }
         }
     }
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int index = getIndex(key);
+        MapEntry<K, V> current = array[index];
+        boolean shrink = false;
+        V value = null;
+        while (true) {
+            if (current == null) break;
+            if (shrink) {
+                array[index - 1] = array[index];
+            }
+            if (current.getKey().equals(key)) {
+                value = current.getValue();
+                shrink = true;
+                array[index] = null;
+            }
+
+            if (++index >= array.length) break;
+            current = array[index];
+        }
+
+        return value;
     }
 
     private int incIndex(int index) {
