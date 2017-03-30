71b0b14c2e0cd59f144eb913ddbdf0e72f3b624f
Sindt <Sindt>
2017-03-30 21:47:22 +0200

    remove added for both

--- src/hashmap/Test.java
+++ src/hashmap/Test.java
@@ -13,19 +13,22 @@ public class Test
 {
     public static void main(String[] args)
     {
-        Map<String, Integer> myMap = new HashMapLink<>();
+        Map<String, Integer> myMap = new HashMap<>();
         
-        for(int i = 0; i < 1000; i += 2)
+        for(int i = 0; i < 11; i += 2)
         {
             String s = "#" + i;
             myMap.put(s, i);
         }
         System.out.println("Size: " + myMap.size());
-        for(int i = 0; i < 1000; ++i)
+        myMap.remove("#8");
+
+        for(int i = 0; i < 11; ++i)
         {
             String s = "#" + i;
             Integer value = myMap.get(s);
             System.out.println("Key: " + s + " Value: " + value);
         }
+        
     }
 }
