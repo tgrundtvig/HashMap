cc8de0f8f05546e56e7f6e7e990a9d2f27de49b5
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-03-30 23:24:51 +0200

    Added a small test

--- src/main/java/hashmap/Test.java
+++ src/main/java/hashmap/Test.java
@@ -8,11 +8,17 @@ public class Test {
             String s = "#" + i;
             myMap.put(s, i);
         }
+
         System.out.println("Size: " + myMap.size());
+
         for (int i = 0; i < 1000; ++i) {
             String s = "#" + i;
             Integer value = myMap.get(s);
             System.out.println("Key: " + s + " Value: " + value);
         }
+
+        System.out.println(myMap.get("#996"));
+        myMap.remove("#996");
+        System.out.println(null == myMap.get("#996") ? "Entry deleted" : "Entry not deleted");
     }
 }
